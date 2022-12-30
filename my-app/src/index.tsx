ba1232e807fdd0b603101d95745fa3b78f4aed1f
Steven ZABANDITH <steven.zabandith@epitech.eu>
2022-12-30 22:52:06 +0100

    [FIX] dom

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/homepage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
